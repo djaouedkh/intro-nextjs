dcccf77c6ccfecbb84bda3277e227b95ba9df5b7
DjaouedKH <74308036+djaouedkh@users.noreply.github.com>
2023-07-11 15:18:20 +0200

    feat log with google account

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 import React, {useState, useEffect} from 'react'
 import styles from './page.module.css'
 import useSWR from 'swr'
+import { useSession } from "next-auth/react";
 
 const Dashboard = () => {
   // const [data, setData] = useState([])
@@ -27,6 +28,9 @@ const Dashboard = () => {
   //   getData()
   // }, [])
 
+  const session = useSession()
+  console.log(session);
+
   const fetcher = (...args) => fetch(...args).then(res => res.json())
 
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
@@ -34,8 +38,6 @@ const Dashboard = () => {
   if (error) return <div>échec du chargement</div>
   if (isLoading) return <div>chargement...</div>
 
-
-  console.log(data);
   return (
     <div className={styles.container}>Dashboard</div>
   )
